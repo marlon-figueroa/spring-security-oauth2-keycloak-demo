66dd8f1fec8771fb2d909417aa27a4df25ea126f
Marlon_Figueroa <marlon.f.1993@gmail.com>
2024-05-24 01:33:24 -0600

    Visualizacion de variables de OAuthUser

--- oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
+++ oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
@@ -1,48 +1,56 @@
 package com.programming.techie.authcode.controller;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
+import net.minidev.json.JSONObject;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.net.http.HttpHeaders;
+import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
     public String home(HttpServletRequest request, ModelMap model) {
-        for (String it: request.getParameterMap().keySet()) {
-            System.out.println(it);
-            System.out.println(Arrays.stream(request.getParameterMap().get(it)).toList());
-            System.out.println("---------------------------");
-        }
-
-        OAuth2User user = ((OAuth2User)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        OAuth2User user = ((OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("usuario", user.getAttribute("name"));
 
-        System.out.println("JSESSIONID" + Arrays.stream(request.getCookies())
-                    .filter(c -> "JSESSIONID".equals(c.getName()))
-                    .map(Cookie::getValue)
-                    .findAny().get());
+        System.out.println(user.getAttributes());
+        Map<String, String> data1 = new HashMap<>();
+
+        JSONObject jsonObject1 = new JSONObject(user.getAttributes());
+        String orgJsonData1 = jsonObject1.toString();
+        model.addAttribute("atributos", orgJsonData1);
+        String arraySplit = Arrays.toString(user.getAuthorities().toArray());
+        if (arraySplit.contains("["))
+            arraySplit = arraySplit.replace("[", "");
+        if (arraySplit.contains("]"))
+            arraySplit = arraySplit.replace("]", "");
+        String[] arrayRoles = arraySplit.split(",");
+        int i = 1;
+        for (String it : arrayRoles) {
+            data1.put("ROL_" + i, it);
+            i++;
+        }
+        JSONObject jsonObject2 = new JSONObject(data1);
+        String orgJsonData2 = jsonObject2.toString();
+        System.out.println(orgJsonData2);
+        model.addAttribute("autorizaciones", orgJsonData2);
 
         return "home";
     }
 
     @GetMapping(path = "/unauthenticated")
     public HashMap unauthenticatedRequests() {
-        return new HashMap(){{
+        return new HashMap() {{
             put("this is ", "unauthenticated endpoint");
         }};
     }
